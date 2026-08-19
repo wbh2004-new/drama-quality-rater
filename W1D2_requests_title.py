afa2ebb45aa52c5f8938da3f508ea9fba23d1d95
wbh2004-new <1967832153@qq.com>
2026-08-19 12:47:03 +0800

    W1-D2: 亲手完成 requests 抓取（函数+try_except）

--- W1D2_requests_title.py
+++ W1D2_requests_title.py
@@ -1,10 +1,14 @@
 import requests
 
-url = "https://www.zhipin.com"
-headers = {
+def get_title(url):
+    headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/151.0.0.0 Safari/537.36"
 }
+    try:
+        resp = requests.get(url, headers=headers, timeout=10)
+        print("状态码:", resp.status_code)
+        print("页面标题:", resp.text[resp.text.find("<title>") + 7 : resp.text.find("</title>")])
+    except requests.exceptions.RequestException as e:
+        print("请求失败了:", e)      
 
-resp = requests.get(url, headers=headers, timeout=10)
-print("状态码:", resp.status_code)
-print("页面标题:", resp.text[resp.text.find("<title>") + 7 : resp.text.find("</title>")])
+get_title("https://www.zhipin.com")
